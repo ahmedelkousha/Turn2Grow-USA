2a091cd4078922f495695c3616528ef85c240416
ahmedelkousha <elkoushaservice@gmail.com>
2026-06-20 19:50:22 +0300

    redeploy

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
+
 export default function NotFound() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-background px-4">
